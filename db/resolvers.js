fd9cef430297b8bd2463dcaa0747e779b82ff9a0
DanielAlejandroGuerrieri <daniel.guerrieri@gmail.com>
2020-11-26 20:24:01 -0300

    Add model Pedido

--- db/resolvers.js
+++ db/resolvers.js
@@ -202,6 +202,27 @@ const resolvers = {
 
             await Cliente.findOneAndDelete({ _id: id });
             return "Cliente Eliminado";
+        },
+        nuevoPedido: async (_,{input},ctx) => {
+            const {cliente} = input;
+            //Verificar si cliente existe
+            let clienteExiste = await Cliente.findById(cliente);
+
+            if(!clienteExiste) throw new Error('Ese cliente no existe');
+
+            //Verificar si el cliente es del vendedor
+            if(clienteExiste.vendedor.toString() !== ctx.usuario.id) {
+                throw new Error('No tienes las credenciales');
+            }
+            //Revisar que el stock este disponible
+
+            //Asignar un vendedor
+
+
+            //Guardarlo en la base de datos
+
+
+
         }
     },
     
